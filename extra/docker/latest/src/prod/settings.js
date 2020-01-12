7c9e8078883ba415663eba8ab721f5a687f4718f
Calogero Crapanzano <calogero.crapanzano@unipa.it>
2020-01-12 16:57:37 +0100

    Modificata icona back

--- extra/docker/latest/src/prod/settings.js
+++ extra/docker/latest/src/prod/settings.js
@@ -23,9 +23,9 @@ module.exports = {
     // Exentriq Settings
 exentriq: {
     clusterZookeeper: "37.187.137.140:5181",
-    boardsApiHost: "boards-new-stage.exentriq.com",
+    boardsApiHost: "boards-new.exentriq.com",
     boardsApiProtocol: "https",
-    rpc: "https://stage.exentriq.com/JSON-RPC"
+    rpc: "https://www.exentriq.com/JSON-RPC"
 },
 
 // the tcp port that the Node-RED web server is listening on
